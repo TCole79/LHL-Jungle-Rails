43a6a82d888452d65df16f6afd7b98476eea5847
TCole79 <tristanleighcole@googlemail.com>
2022-02-05 17:14:15 +0000

    Implement Rails Example part 3

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,5 +1,10 @@
 class Sale < ActiveRecord::Base
 
+  #create class methods, this is creating an Active Record scope
+  def self.active?
+    where("sales.starts_on <= ? AND sales.ends_on >= ?", Date.current, Date.current).any?
+  end
+
   def finished?
     ends_on < Date.current
   end
