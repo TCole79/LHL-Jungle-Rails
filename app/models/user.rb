16ba71acf4cefd55ebb802e7a48d238057bbc87b
TCole79 <tristanleighcole@googlemail.com>
2022-02-08 22:15:40 +0000

    Implement Model spec: User tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,21 @@ class User < ActiveRecord::Base
 
   has_secure_password
   
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: {case_sensitive: false} 
+  validates :password, length: { minimum: 5 }
+
+  def self.authenticate_with_credentials(email, password)
+
+    user = User.find_by_email(email.strip.downcase) # strip removes whitespace, downcase makes all text lowercase
+
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+
+  end
+
 end
